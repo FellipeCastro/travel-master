98381185d9302515b808475cdecd83403957cfd5
Fellipe Castro <fellipecastro27d@gmail.com>
2024-09-25 18:10:54 -0300

    Projeto iniciado

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import Home from './components/pages/Home'
+import Company from './components/Company'
+import Contact from './components/pages/Contact'
+import NewProject from './components/pages/NewProject'
+import Container from './components/layout/Container'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
+    <Router>
+      <ul>
+        <Link to="/">Home</Link>
+        <Link to="/company">Empresa</Link>
+        <Link to="/contact">Contato</Link>
+        <Link to="/newproject">Novo Projeto</Link>
+      </ul>
+
+      <Container customClass="min-height">
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/company" element={<Company />} />
+          <Route exact path="/contact" element={<Contact />} />
+          <Route exact path="/newproject" element={<NewProject />} />
+        </Routes>
+      </Container>
+
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        Footer
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+    </Router>
   )
 }
 
