2e218bd129949668cdb34cde523e9d42f55bacf9
Fellipe Castro <fellipecastro27d@gmail.com>
2024-09-25 20:57:59 -0300

    Home finalizada

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,8 +1,16 @@
+import styles from "./Home.module.css"
+import TripBro from "../../img/trip-bro.svg"
+import LinkButton from "../layout/LinkButton"
+
 function Home() {
     return (
-        <>
-            <h1>Home</h1>
-        </>
+        <section className={styles.home_container}>
+            <h1>Bem-vindo ao <span>Travel Master</span></h1>
+            <p>Começe a gerenciar seu pacotes de viagens</p>
+            <LinkButton to="/newpackage" text="Criar pacote" />
+
+            <img src={TripBro} alt="Homem viajando" />
+        </section>
     )
 }
 
