03029415c67f6d8d79bb4a6a01ec097461613ae9
Fellipe Castro <fellipecastro27d@gmail.com>
2024-10-02 14:11:15 -0300

    Projeto atualizado

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,25 +1,20 @@
 import { Link } from 'react-router-dom'
 
-import Container from './Container'
-
 import styles from './Navbar.module.css'
-import logo from '../../img/logo.png'
 
 function Navbar() {
     return (
         <nav className={styles.navbar}>
-            <Container>
-                <Link to="/">
-                    <img src={logo} alt="Logo" />
-                </Link>
+            <Link to="/" className={styles.logo}>
+                Travel Master
+            </Link>
 
-                <ul className={styles.list}>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/packages">Pacotes</Link></li>
-                    <li><Link to="/company">Empresa</Link></li>
-                    <li><Link to="/contact">Contato</Link></li>
-                </ul>
-            </Container>
+            <ul className={styles.list}>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/packages">Pacotes</Link></li>
+                <li><Link to="/company">Empresa</Link></li>
+                <li><Link to="/contact">Contato</Link></li>
+            </ul>
         </nav>
     )
 }
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,15 +1,15 @@
 import styles from "./Home.module.css"
-import TripBro from "../../img/trip-bro.svg"
+import HomeImg from "../../img/home-img.svg"
 import LinkButton from "../layout/LinkButton"
 
 function Home() {
     return (
         <section className={styles.home_container}>
             <h1>Bem-vindo ao <span>Travel Master</span></h1>
-            <p>Começe a gerenciar seu pacotes de viagens</p>
+            <p>Começe a gerenciar seu pacotes de viagens agora mesmo!</p>
             <LinkButton to="/newpackage" text="Criar pacote" />
 
-            <img src={TripBro} alt="Homem viajando" />
+            <img src={HomeImg} alt="Homem viajando" />
         </section>
     )
 }
